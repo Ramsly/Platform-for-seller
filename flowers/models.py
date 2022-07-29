cf2158035baea634a33d1af6fa1bacd263ed4cdc
Ramil2003 <theluckyfeed1@gmail.com>
2022-07-29 09:21:30 +0300

    Add Seller and Buyer models. Add gitignore

--- flowers/models.py
+++ flowers/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Seller(models.Model):
+    seller = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Buyer(models.Model):
+    buyer = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
